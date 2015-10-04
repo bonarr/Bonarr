337097833fcdcec4967f3c3db647705f25b00ac5
Mark McDowall <markus.mcd5@gmail.com>
2015-10-04 13:33:11 -0700

    Show time instead of date if event occurs/occurred today

--- frontend/src/Cells/RelativeDateCell.js
+++ frontend/src/Cells/RelativeDateCell.js
@@ -4,21 +4,29 @@ var FormatHelpers = require('../Shared/FormatHelpers');
 var UiSettings = require('../Shared/UiSettingsModel');
 
 module.exports = NzbDroneCell.extend({
-  className: 'relative-date-cell',
+  className : 'relative-date-cell',
+  showSeconds : false,
+  showSecondsOnTooltip : false,
 
-  render: function() {
+  render : function() {
+
     var dateStr = this.model.get(this.column.get('name'));
 
     if (dateStr) {
       var date = moment(dateStr);
+      var diff = date.diff(moment().zone(date.zone()).startOf('day'), 'days', true);
       var result = '<span title="{0}">{1}</span>';
-      var tooltip = date.format(UiSettings.longDateTime());
+      var tooltip = date.format(UiSettings.longDateTime(this.showSecondsOnTooltip));
       var text;
 
-      if (UiSettings.get('showRelativeDates')) {
-        text = FormatHelpers.relativeDate(dateStr);
+      if (diff > 0 && diff < 1) {
+        text = date.format(UiSettings.time(true, this.showSeconds));
       } else {
-        text = date.format(UiSettings.get('shortDateFormat'));
+        if (UiSettings.get('showRelativeDates')) {
+          text = FormatHelpers.relativeDate(dateStr);
+        } else {
+          text = date.format(UiSettings.get('shortDateFormat'));
+        }
       }
 
       this.$el.html(result.format(tooltip, text));
--- frontend/src/System/Logs/Table/LogTimeCell.js
+++ frontend/src/System/Logs/Table/LogTimeCell.js
@@ -1,14 +1,6 @@
-var NzbDroneCell = require('../../../Cells/NzbDroneCell');
-var moment = require('moment');
-var UiSettings = require('../../../Shared/UiSettingsModel');
+var NzbDroneCell = require('../../../Cells/RelativeDateCell');
 
 module.exports = NzbDroneCell.extend({
-  className: 'log-time-cell',
-
-  render: function() {
-    var date = moment(this._getValue());
-    this.$el.html('<span title="{1}">{0}</span>'.format(date.format(UiSettings.time(true, false)), date.format(UiSettings.longDateTime(true))));
-
-    return this;
-  }
+  className : 'log-time-cell',
+  showSecondsOnTooltip : true
 });
