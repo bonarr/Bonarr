a9fdcba0cf9d284e2d86800fa2abc6196ced619d
Keivan Beigi <me@keivan.io>
2015-11-26 17:36:22 -0800

    fixed zero.clipboard

--- frontend/src/Mixins/CopyToClipboard.js
+++ frontend/src/Mixins/CopyToClipboard.js
@@ -1,21 +1,16 @@
 var $ = require('jquery');
-var StatusModel = require('../System/StatusModel');
 var ZeroClipboard = require('zero.clipboard');
 var Messenger = require('../Shared/Messenger');
 
 $.fn.copyToClipboard = function(input) {
   ZeroClipboard.config({
-    swfPath: StatusModel.get('urlBase') + '/Content/zero.clipboard.swf'
+    swfPath: `${window.Sonarr.UrlBase}/Content/Vendor/zero.clipboard.swf`
   });
 
   var client = new ZeroClipboard(this);
 
-  client.on('ready', function(e) {
-    client.on('copy', function(e) {
-      e.clipboardData.setData("text/plain", input.val());
-    });
-    client.on('aftercopy', function() {
-      Messenger.show({ message: 'Copied text to clipboard' });
-    });
+  client.on('ready', () => {
+    client.on('copy', (e) => e.clipboardData.setData('text/plain', input.val()));
+    client.on('aftercopy', () => Messenger.show({ message: 'Copied text to clipboard' }));
   });
 };
