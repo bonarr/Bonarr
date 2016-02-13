2ccad978d371974c0a5702f810dab42b74d167b4
Keivan Beigi <me@keivan.io>
2016-02-12 21:21:04 -0800

    much saner webpack config

--- frontend/gulp/webpack.js
+++ frontend/gulp/webpack.js
@@ -19,40 +19,20 @@ var config = {
     main: 'main.js'
   },
   resolve: {
-    root: root,
+    root: [
+      root,
+      path.join(root, 'Shims'),
+      path.join(root, 'JsLibraries')
+    ],
     alias: {
-      backbone: 'Shims/backbone',
       'backbone-pageable': 'JsLibraries/backbone.pageable',
-      'backbone.collectionview': 'Shims/backbone.collectionview',
       'backbone.deepmodel': 'Shims/backbone.deep.model',
-      'backbone.modelbinder': 'JsLibraries/backbone.modelbinder',
-      'backbone.pageable': 'JsLibraries/backbone.pageable',
-      'backbone.paginator': 'JsLibraries/backbone.paginator',
       'backbone.sorted.collection': 'JsLibraries/backbone-sorted-collection',
       'radio': 'JsLibraries/backbone.radio',
-      'backbone.validation': 'Shims/backbone.validation',
-      'backgrid': 'Shims/backgrid',
-      'backgrid.paginator': 'Shims/backgrid.paginator',
       'backgrid.selectall': 'Shims/backbone.backgrid.selectall',
-      'bootstrap': 'JsLibraries/bootstrap',
-      'bootstrap.tagsinput': 'JsLibraries/bootstrap.tagsinput',
-      'filesize': 'JsLibraries/filesize',
-      'handlebars': 'Shims/handlebars',
-      'handlebars.helpers': 'JsLibraries/handlebars.helpers',
-      'jquery': 'Shims/jquery',
-      'jquery-ui': 'JsLibraries/jquery-ui',
-      'jquery.dotdotdot': 'JsLibraries/jquery.dotdotdot',
-      'jquery.easypiechart': 'JsLibraries/jquery.easypiechart',
-      'jquery.knob': 'JsLibraries/jquery.knob',
-      'jquery.lazyload': 'JsLibraries/jquery.lazyload',
       'marionette' : 'Shims/backbone.marionette',
-      'messenger': 'Shims/messenger',
-      'moment': 'JsLibraries/moment',
       'momentRange': 'JsLibraries/moment-range',
-      'signalR': 'Shims/jquery.signalR',
-      'typeahead': 'JsLibraries/typeahead',
-      'underscore': 'Shims/underscore',
-      'zero.clipboard': 'JsLibraries/zero.clipboard'
+      'signalR': 'Shims/jquery.signalR'
     }
   },
   output: {
