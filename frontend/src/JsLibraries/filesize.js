19497fbc784487de04e6b94a727a57a857ad8d19
Keivan Beigi <me@keivan.io>
2016-04-14 22:31:16 -0700

    filesize.js

--- frontend/src/JsLibraries/filesize.js
+++ frontend/src/JsLibraries/filesize.js
@@ -3,21 +3,18 @@
 /**
  * filesize
  *
- * @author Jason Mulligan <jason.mulligan@avoidwork.com>
- * @copyright 2015 Jason Mulligan
- * @license BSD-3 <https://raw.github.com/avoidwork/filesize.js/master/LICENSE>
- * @link http://filesizejs.com
- * @module filesize
- * @version 3.1.2
+ * @copyright 2016 Jason Mulligan <jason.mulligan@avoidwork.com>
+ * @license BSD-3-Clause
+ * @version 3.2.1
  */
-(function(global) {
-  var bit = /b$/;
-  var si = {
-    bits: ["B", "kb", "Mb", "Gb", "Tb", "Pb", "Eb", "Zb", "Yb"],
-    bytes: ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-  };
+(function (global) {
+	var b = /^(b|B)$/;
+	var symbol = {
+		bits: ["b", "Kb", "Mb", "Gb", "Tb", "Pb", "Eb", "Zb", "Yb"],
+		bytes: ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+	};
 
-  /**
+	/**
   * filesize
   *
   * @method filesize
@@ -25,132 +22,119 @@
   * @param  {Object}  descriptor [Optional] Flags
   * @return {String}             Readable file size String
   */
-  var filesize = function(arg) {
-    var descriptor = arguments[1] === undefined ? {} : arguments[1];
+	function filesize(arg) {
+		var descriptor = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 
-    var result = [];
-    var skip = false;
-    var val = 0;
-    var e = undefined,
-      base = undefined,
-      bits = undefined,
-      ceil = undefined,
-      neg = undefined,
-      num = undefined,
-      output = undefined,
-      round = undefined,
-      unix = undefined,
-      spacer = undefined,
-      suffixes = undefined;
+		var result = [],
+		    val = 0,
+		    e = undefined,
+		    base = undefined,
+		    bits = undefined,
+		    ceil = undefined,
+		    neg = undefined,
+		    num = undefined,
+		    output = undefined,
+		    round = undefined,
+		    unix = undefined,
+		    spacer = undefined,
+		    symbols = undefined;
 
-    if (isNaN(arg)) {
-      throw new Error("Invalid arguments");
-    }
+		if (isNaN(arg)) {
+			throw new Error("Invalid arguments");
+		}
 
-    bits = descriptor.bits === true;
-    unix = descriptor.unix === true;
-    base = descriptor.base !== undefined ? descriptor.base : 2;
-    round = descriptor.round !== undefined ? descriptor.round : unix ? 1 : 2;
-    spacer = descriptor.spacer !== undefined ? descriptor.spacer : unix ? "" : " ";
-    suffixes = descriptor.suffixes !== undefined ? descriptor.suffixes : {};
-    output = descriptor.output !== undefined ? descriptor.output : "string";
-    e = descriptor.exponent !== undefined ? descriptor.exponent : -1;
-    num = Number(arg);
-    neg = num < 0;
-    ceil = base > 2 ? 1000 : 1024;
+		bits = descriptor.bits === true;
+		unix = descriptor.unix === true;
+		base = descriptor.base || 2;
+		round = descriptor.round !== undefined ? descriptor.round : unix ? 1 : 2;
+		spacer = descriptor.spacer !== undefined ? descriptor.spacer : unix ? "" : " ";
+		symbols = descriptor.symbols || descriptor.suffixes || {};
+		output = descriptor.output || "string";
+		e = descriptor.exponent !== undefined ? descriptor.exponent : -1;
+		num = Number(arg);
+		neg = num < 0;
+		ceil = base > 2 ? 1000 : 1024;
 
-    // Flipping a negative number to determine the size
-    if (neg) {
-      num = -num;
-    }
+		// Flipping a negative number to determine the size
+		if (neg) {
+			num = -num;
+		}
 
-    // Zero is now a special case because bytes divide by 1
-    if (num === 0) {
-      result[0] = 0;
+		// Zero is now a special case because bytes divide by 1
+		if (num === 0) {
+			result[0] = 0;
+			result[1] = unix ? "" : !bits ? "B" : "b";
+		} else {
+			// Determining the exponent
+			if (e === -1 || isNaN(e)) {
+				e = Math.floor(Math.log(num) / Math.log(ceil));
 
-      if (unix) {
-        result[1] = "";
-      } else {
-        result[1] = "B";
-      }
-    } else {
-      // Determining the exponent
-      if (e === -1 || isNaN(e)) {
-        e = Math.floor(Math.log(num) / Math.log(ceil));
-      }
+				if (e < 0) {
+					e = 0;
+				}
+			}
 
-      // Exceeding supported length, time to reduce & multiply
-      if (e > 8) {
-        val = val * (1000 * (e - 8));
-        e = 8;
-      }
+			// Exceeding supported length, time to reduce & multiply
+			if (e > 8) {
+				e = 8;
+			}
 
-      if (base === 2) {
-        val = num / Math.pow(2, e * 10);
-      } else {
-        val = num / Math.pow(1000, e);
-      }
+			val = base === 2 ? num / Math.pow(2, e * 10) : num / Math.pow(1000, e);
 
-      if (bits) {
-        val = val * 8;
+			if (bits) {
+				val = val * 8;
 
-        if (val > ceil) {
-          val = val / ceil;
-          e++;
-        }
-      }
+				if (val > ceil && e < 8) {
+					val = val / ceil;
+					e++;
+				}
+			}
 
-      result[0] = Number(val.toFixed(e > 0 ? round : 0));
-      result[1] = si[bits ? "bits" : "bytes"][e];
+			result[0] = Number(val.toFixed(e > 0 ? round : 0));
+			result[1] = base === 10 && e === 1 ? bits ? "kb" : "kB" : symbol[bits ? "bits" : "bytes"][e];
 
-      if (!skip && unix) {
-        if (bits && bit.test(result[1])) {
-          result[1] = result[1].toLowerCase();
-        }
+			if (unix) {
+				result[1] = result[1].charAt(0);
 
-        result[1] = result[1].charAt(0);
+				if (b.test(result[1])) {
+					result[0] = Math.floor(result[0]);
+					result[1] = "";
+				}
+			}
+		}
 
-        if (result[1] === "B") {
-          result[0] = Math.floor(result[0]);
-          result[1] = "";
-        } else if (!bits && result[1] === "k") {
-          result[1] = "K";
-        }
-      }
-    }
+		// Decorating a 'diff'
+		if (neg) {
+			result[0] = -result[0];
+		}
 
-    // Decorating a 'diff'
-    if (neg) {
-      result[0] = -result[0];
-    }
+		// Applying custom suffix
+		result[1] = symbols[result[1]] || result[1];
 
-    // Applying custom suffix
-    result[1] = suffixes[result[1]] || result[1];
+		// Returning Array, Object, or String (default)
+		if (output === "array") {
+			return result;
+		}
 
-    // Returning Array, Object, or String (default)
-    if (output === "array") {
-      return result;
-    }
+		if (output === "exponent") {
+			return e;
+		}
 
-    if (output === "exponent") {
-      return e;
-    }
+		if (output === "object") {
+			return { value: result[0], suffix: result[1], symbol: result[1] };
+		}
 
-    if (output === "object") {
-      return { value: result[0], suffix: result[1] };
-    }
+		return result.join(spacer);
+	}
 
-    return result.join(spacer);
-  };
-
-  // CommonJS, AMD, script tag
-  if (typeof exports !== "undefined") {
-    module.exports = filesize;
-  } else if (typeof define === "function") {
-    define(function() {
-      return filesize;
-    });
-  } else {
-    global.filesize = filesize;
-  }
-})(typeof global !== "undefined" ? global : window);
+	// CommonJS, AMD, script tag
+	if (typeof exports !== "undefined") {
+		module.exports = filesize;
+	} else if (typeof define === "function" && define.amd) {
+		define(function () {
+			return filesize;
+		});
+	} else {
+		global.filesize = filesize;
+	}
+})(typeof window !== "undefined" ? window : global);
