da587ac634bb83bfb2a655b89abc76590676e655
Mark McDowall <markus.mcd5@gmail.com>
2016-11-05 17:57:04 -0700

    Remove old indexer views/models

--- frontend/src/Settings/Indexer/Add/IndexerAddCollectionView.js
+++
@@ -1,9 +0,0 @@
-var ThingyAddCollectionView = require('../../ThingyAddCollectionView');
-var ThingyHeaderGroupView = require('../../ThingyHeaderGroupView');
-var AddItemView = require('./IndexerAddItemView');
-
-module.exports = ThingyAddCollectionView.extend({
-  childView: ThingyHeaderGroupView.extend({ childView: AddItemView }),
-  childViewContainer: '.add-indexer .items',
-  template: 'Settings/Indexer/Add/IndexerAddCollectionViewTemplate'
-});
--- frontend/src/Settings/Indexer/IndexerSettingsModel.js
+++
@@ -1,7 +0,0 @@
-var SettingsModelBase = require('../SettingsModelBase');
-
-module.exports = SettingsModelBase.extend({
-  url: '/config/indexer',
-  successMessage: 'Indexer settings saved',
-  errorMessage: 'Failed to save indexer settings'
-});
