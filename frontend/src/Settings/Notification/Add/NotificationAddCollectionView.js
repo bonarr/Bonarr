0a1ab545a62f0d07e76b3b787266b506bf24c0e5
Mark McDowall <markus.mcd5@gmail.com>
2016-11-08 23:53:26 -0800

    Remove old notifications

--- frontend/src/Settings/Notification/Add/NotificationAddCollectionView.js
+++
@@ -1,8 +0,0 @@
-var ThingyAddCollectionView = require('../../ThingyAddCollectionView');
-var AddItemView = require('./NotificationAddItemView');
-
-module.exports = ThingyAddCollectionView.extend({
-  childView: AddItemView,
-  childViewContainer: '.add-notifications .items',
-  template: 'Settings/Notification/Add/NotificationAddCollectionViewTemplate'
-});
