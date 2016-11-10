6da89b3e82eaaaf886ee46b3fbb7be6ff3e02949
Mark McDowall <markus.mcd5@gmail.com>
2016-11-10 00:02:39 -0800

    Remove old metadata settings

--- frontend/src/Settings/Metadata/MetadataCollectionView.js
+++
@@ -1,8 +0,0 @@
-var Marionette = require('marionette');
-var MetadataItemView = require('./MetadataItemView');
-
-module.exports = Marionette.CompositeView.extend({
-  childView: MetadataItemView,
-  childViewContainer: '#x-metadata',
-  template: 'Settings/Metadata/MetadataCollectionView'
-});
