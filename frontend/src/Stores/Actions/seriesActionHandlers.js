c179631faf526a0688e46d61cf6680834fe9a5d0
Mark McDowall <markus.mcd5@gmail.com>
2016-12-21 23:25:09 -0800

    fixup! Reactified Series Index

--- frontend/src/Stores/Actions/seriesActionHandlers.js
+++ frontend/src/Stores/Actions/seriesActionHandlers.js
@@ -1,41 +1,10 @@
-import $ from 'jquery';
 import * as types from './actionTypes';
+import createFetchHandler from './Creators/createFetchHandler';
 import createSaveProviderHandler from './Creators/createSaveProviderHandler';
 import createRemoveItemHandler from './Creators/createRemoveItemHandler';
-import { set, update } from './baseActions';
-
-const section = 'series';
 
 const seriesActionHandlers = {
-  [types.FETCH_SERIES]: function(payload) {
-    return function(dispatch, getState) {
-      dispatch(set({ section, fetching: true }));
-
-      const promise = $.ajax({
-        url: '/series'
-      });
-
-      promise.done((data) => {
-        dispatch(update({ section, data }));
-
-        dispatch(set({
-          section,
-          fetching: false,
-          populated: true,
-          error: null
-        }));
-      });
-
-      promise.fail((xhr) => {
-        dispatch(set({
-          section,
-          fetching: false,
-          populated: false,
-          error: xhr
-        }));
-      });
-    };
-  },
+  [types.FETCH_SERIES]: createFetchHandler('series', '/series'),
 
   [types.SAVE_SERIES]: createSaveProviderHandler('series',
                                                  '/series',
