aba730bb42652e0af364dec330c68eb073c59f14
Mark McDowall <markus.mcd5@gmail.com>
2016-12-23 14:44:28 -0800

    fixup! Reactified add new series

--- frontend/src/AddSeries/AddNewSeries/SearchResultCollectionView.js
+++
@@ -1,7 +0,0 @@
-const Marionette = require('marionette');
-const SearchResultItemView = require('./SearchResultItemView');
-const SearchResultCollectionView = Marionette.CollectionView.extend({
-  childView: SearchResultItemView
-});
-
-module.exports = SearchResultCollectionView;
